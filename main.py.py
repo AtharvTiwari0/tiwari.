b9904af06961f4a869afd9b6c0c47390683d05f5
AtharvTiwari0 <atharv.tiwari.012@gmail.com>
2024-12-14 14:05:50 +0530

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,20 @@
+import  streamlit as st
+
+st.title("*WELCOME* IN MY WEBSITE "
+         " MY NAME IS ATHARV ")
+st.header("MY QILIFICATION ARE :-")
+st.text("I AM STUDY IN CLASS 7TH ")
+st.text("I AM 6TH PASS...")
+st.text("IN ALL CLASS WE SCORE ABOBE 79.9%")
+st.text("I AM ALSO USE PYTHON")
+st.header("MY HOBIES ARE :-")
+st.text("TO PLAY GTA 5")
+st.text("I LIKE CIRCKET AND TEAM INDIA ")
+st.text("LIKE CYCLEING")
+st.text("TO PLAY OUT DOOR GAME LIKE CIRCKET,FOOTBALL etc.......")
+st.header("SOME EXTRA INFORMATION ABOUT ME ")
+st.text("I AM LIVING IN INDIA ")
+st.text("IN INDIA MY HOME TOWN IN UTTER PRADESH")
+st.text("WE PROMOTE CHAU BAZI AND ALSO HIS LEADER")
+st.text("MY AND CHAU BAZI LEADER IS ONLY ONE "
+        "IS---- ANJNEY")
